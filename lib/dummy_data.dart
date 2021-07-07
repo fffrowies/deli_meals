56aaf5492444817f6ab40d5bf09879052bdda943
ffrowies <fernandorowies.saxo@gmail.com>
2021-07-07 06:25:16 -0300

    Used named routes, (cont)

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -53,4 +53,14 @@ const DUMMY_CATEGORIES = const [
     title: 'Summer',
     color: Colors.teal,
   ),
+  Category(
+    id: 'c11',
+    title: 'Argentine',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Mexican',
+    color: Colors.indigo,
+  ),
 ];
