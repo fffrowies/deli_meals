970293dd956e39833360ca69c7ecc8b828aa510b
ffrowies <fernandorowies.saxo@gmail.com>
2021-07-08 08:58:20 -0300

    Used replacing pages for accesed from drawer.

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('The Favorites'),
+    return Scaffold(
+      body: Center(
+        child: Text('The Favorites'),
+      ),
     );
   }
 }
